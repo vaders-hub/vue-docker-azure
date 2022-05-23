d0d9feb08d6f400960e8cb5ee21deba7ba72c500
chdk921 <chdk921@gmail.com>
2022-05-23 14:47:31 +0900

    remove file

--- front/src/views/Scope_3.vue
+++ front/src/views/Scope_3.vue
@@ -1,16 +1,10 @@
 <script lang="ts">
 import { defineComponent, onMounted, reactive, watch } from 'vue'
 import { useMainStore } from '@/store/index'
-import Pie from '@/components/chart/Pie.vue'
-import Doughnut from '@/components/chart/Doughnut.vue'
-import StackedBar from '../components/chart/StackedBar.vue'
 
 export default defineComponent({
   name: 'Scope_3',
-  components: {
-    Pie,
-    Doughnut,
-},
+  components: {},
   setup(context) {
     const mainStore = useMainStore()
 
@@ -23,8 +17,4 @@ export default defineComponent({
 
 <template>
   <h2>Scope 3</h2>
-  <Pie />
-  <br><br><br>
-  <Doughnut />
-  <br><br><br>
 </template>
